1519219ac97043d6ffb9453ed4693620368674dd
Felipe Girotti <felipe.girotti@gmail.com>
2019-05-21 00:14:05 -0300

    Third part, added propTypes and axios to access the todo api

--- src/components/todo/index.js
+++ src/components/todo/index.js
@@ -1,30 +1,44 @@
 import React, { Component } from 'react';
 import './todo.css';
 import TodoList from './../todolist'
+import Api from './../../service/api/todo'
 
 class Todo extends Component {
-    state = { todo: '', items: [{ id: 1, title: 'alguma coisa', done: false }] }
+    state = { todo: '', disabled: false, items: [{ id: 1, title: 'alguma coisa', done: false }] }
 
-    handleSubmit = (event) => {
+    componentDidMount() {
+        Api.get()
+            .then(response => (this.setState({ ...this.state, items: response.data })));
+    }
+
+    handleSubmit = async (event) => {
         event.preventDefault();
-        const items = [...this.state.items, { id: Math.random(), title: this.state.todo, done: false }];
+        this.setState({...this.state, loading: true});
+        const response = await Api.post('', {title: this.state.todo, done: false})        
+        const items = [...this.state.items];
+        items.unshift(response.data);
         this.setState({ todo: '', items: items });
+        this.setState({...this.state, loading: false});
     }
     handleDone = (id) => {
+        this.setState({ ...this.state, loading: true });
         const items = this.state.items
-            .map(i => {
+            .map((i) => {
                 if (i.id === id) {
+                    Api.put('/' + id, {title: i.title, done: true})
+                        .finally(() => this.setState({ ...this.state, loading: false }));
                     i.done = true;
                 }
                 return i;
-            });
-
+            });        
         this.setState({ ...this.state, items: items });
     }
 
-    handleDelete = (id) => {
+    handleDelete = async (id) => {
+        this.setState({ ...this.state, loading: true });
+        await Api.delete('/' + id);
         const items = this.state.items.filter(i => i.id !== id);
-        this.setState({ ...this.state, items: items });
+        this.setState({ ...this.state, loading: false, items: items });        
     }
 
     render() {
@@ -38,8 +52,8 @@ class Todo extends Component {
                         <div className="row">
                             <div className="form-control">
                                 <label>Tarefa</label>
-                                <input type="text" placeholder="EX: Estudar React" onChange={(e) => this.setState({ ...this.state, todo: e.target.value })} value={this.state.todo}></input>
-                                <button type="submit">OK</button>
+                                <input type="text" placeholder="EX: Estudar React" disabled={this.state.loading} onChange={(e) => this.setState({ ...this.state, todo: e.target.value })} value={this.state.todo}></input>
+                                <button type="submit" disabled={this.state.loading}>OK</button>
                             </div>
                         </div>
 
--- src/components/todolist/index.js
+++ src/components/todolist/index.js
@@ -1,12 +1,13 @@
 import React, {Fragment} from 'react';
+import PropTypes from 'prop-types';
 
 const TodoList = ({items, handleDone, handleDelete}) => (
     <Fragment>
         <h2>Lista</h2>
         <ul>
             {items.map(todo => (
-                <Fragment>
-                    <li key={todo.id} className={todo.done ? 'line-strike' : ''}>{todo.title}</li>
+                <Fragment key={todo.id}>
+                    <li className={todo.done ? 'line-strike' : ''}>{todo.title}</li>
                     <button onClick={() => handleDone(todo.id)}>Terminado</button>
                     <button onClick={() => handleDelete(todo.id)}>Apagar</button>
                 </Fragment>
@@ -14,5 +15,15 @@ const TodoList = ({items, handleDone, handleDelete}) => (
         </ul>
     </Fragment>
 ); 
+
+TodoList.propTypes = {
+    items: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        done: PropTypes.bool.isRequired,
+    })),
+    handleDelete: PropTypes.func.isRequired,
+    handleDone: PropTypes.func.isRequired,
+};
 
 export default TodoList;
